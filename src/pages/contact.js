1f4a4a363c250365dfc9951229f1531f128540c1
crissesoto <soto.crisse@hotmail.com>
2021-10-08 12:07:27 +0200

    fixed contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,14 +11,14 @@ export default function Contact() {
 			description="Learn more about me"
 		/>
 		<section class="hero is-fullheight">
-				<div class="hero-body">
+				<div>
 					<div class="container has-text-centered">
 						<div class="columns is-8 is-variable ">
-							<div class="column is-two-thirds has-text-left">
+							<div class="column is-block has-text-left">
 								<h1 class="title is-1">Contact Me</h1>
 								<p class="is-size-4">Currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!.</p>
 							</div>
-							<div class="column is-one-third has-text-left">
+							<div class="column is-block has-text-left">
 								<div class="field">
 									<label class="label">Name
 										<div class="control">
